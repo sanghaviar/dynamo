c3fedb9c9e9ba3e20f65bbc2755e5693fa2c3857
sanghavi <sanghavi.a.r@diggibyte.com>
2024-04-12 19:16:38 +0530

    resources

--- main.tf
+++ main.tf
@@ -6,3 +6,8 @@ module "dynamo_db" {
   for_each      = {for entry in local.data["dynamo_db"] : entry["db_table_name"] => entry}
   config = each.value
 }
+module "s3_bucket" {
+  source = "./modules/s3"
+  for_each      = {for entry in local.data["s3"] : entry["aws_s3_bucket_name"] => entry}
+  config = each.value
+}
